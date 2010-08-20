d862cfd250647d6449fcbde897b2871f82e1200f
Wong Liang Zan <zan@liangzan.net>
2010-08-20 17:53:15 +0800

    first draft of the integration with live validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  validates_presence_of :name, :mobile
+  validates_presence_of :name, :message => "its not right baby"
   validates_uniqueness_of :email
   validates_length_of :mobile, :is => 4, :message => 'its wrong'
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,87 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+/* find all the inputs,
+ * for each input
+ * read the attributes
+ * and bind the input field to the corresponding live validation function
+ */
+function initialize() {
+    var textInputs = selectInputElements();
+    var validationType, validationOptions;
+    for (var i = 0; i < textInputs.length; i++) {
+	validationOptions = getValidationAttributes(textInputs[i]);
+	validationType = textInputs[i].getAttribute("validation");
+	bindInputElements(textInputs[i], validationType, validationOptions);
+    }
+}
+
+function selectInputElements() {
+    var inputElements = document.getElementsByTagName('input');
+    var textInputs = new Array;
+    for (var i = 0; i < inputElements.length; i++) {
+	var element = inputElements[i];
+	var inputType = element.getAttribute("type");
+	if (inputType == "text") {
+	    textInputs.push(inputElements[i]);
+	}
+    }
+    return textInputs;    
+}
+
+function getValidationAttributes(element) {
+    var validationOptions = {};
+    var elementAttributes = element.attributes;
+    var validationKey, validationValue;
+    for(var i = 0; i < elementAttributes.length; i++) {
+	validationKey = extractValidationKey(elementAttributes[i].name);
+	validationValue = elementAttributes[i].value;
+	if (validationKey != null) {
+	    validationOptions[validationKey] = validationValue;
+	}
+    }
+    return validationOptions;
+}
+
+function extractValidationKey(key) {
+    var namespaceRegex = /validation_(\w+)/;
+    var result = key.match(namespaceRegex);
+    return result != null ? result[1] : null;
+}
+
+function bindInputElements(element, validation, options) {
+    alert(element.id + ":" + validation);
+    var elementValidation = new LiveValidation(element);
+    
+    switch(validation) {
+    case "validates_acceptance_of":
+	break;
+    case "validates_associated":
+	break;
+    case "validates_confirmation_of":
+	break;
+    case "validates_confirmation_of":
+	break;
+    case "validates_each":
+	break;
+    case "validates_exclusion_of":
+	break;
+    case "validates_format_of":
+	elementValidation.add(Validate.Format, {pattern: new RegExp(options["with"]), failureMessage: options["message"]});
+	break;
+    case "validates_inclusion_of":
+	break;
+    case "validates_length_of":
+	break;
+    case "validates_numericality_of":
+	elementValidation.add(Validate.Numericality, {is: options["equal_to"], minimum: options["greater_than_or_equal_to"], maximum: options["less_than_or_equal_to"], onlyinteger: options["only_integer"], failureMessage: options["message"]});
+	break;
+    case "validates_presence_of":
+	elementValidation.add(Validate.Presence, {failureMessage: options["message"]});
+	break;
+    case "validates_size_of":
+	break;
+    case "validates_uniqueness_of":
+	break;
+    }
+}
