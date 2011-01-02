940cbd86284fdf7239e38bcc6c7cfd1e69e1c529
Wong Liang Zan <zan@liangzan.net>
2011-01-03 07:51:50 +0800

    downgraded to formtastic 1

--- config/environment.rb
+++ config/environment.rb
@@ -22,7 +22,7 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem 'liangzan-validation_reflection', :lib => 'validation_reflection', :source  => GEMCUTTER_GEM_SRC, :version => '0.3.9'
-  config.gem 'formtastic', :source  => GEMCUTTER_GEM_SRC, :version => '1.1.0'
+  config.gem 'formtastic', :source  => GEMCUTTER_GEM_SRC, :version => '1.0.0'
   config.gem 'formtastic_validation', :source  => GEMCUTTER_GEM_SRC, :version => '0.1.2'
   config.gem 'cucumber', :source  => GEMCUTTER_GEM_SRC, :version => '0.8.5'
 
